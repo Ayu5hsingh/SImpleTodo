5be2e25034feb9518336166c66a49d4955eceada
Ayush Singh <heyayushsingh05@gmail.com>
2024-01-06 02:46:00 +0530

    changes made to add todo

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,9 +1,25 @@
+import axios from "axios"
+import { useState } from "react"
+
 export function CreateTodo() {
+    const [description,setDescription]=useState("")
+    const [title,setTitle]=useState("")
+    async function sendData() {
+        const response = await axios.post('http://localhost:3000/todos',
+            {
+                title: title,
+                description: description
+            }
+        )
+        console.log(response)
+    }
+
     return (
+        // work on this and make the frontend send request to the backend.
         <div>
-            <input type="text" placeholder="Title" /> <br />
-            <input type="text" placeholder="Description"/> <br />
-            <button>Add Todo</button>
+            <input id="title" type="text" value={title} placeholder="Title" onChange={(e)=>{setTitle(e.target.value)}} /> <br />
+            <input id="description" type="text" value={description} placeholder="Description" onChange={(e)=>{setDescription(e.target.value)}}/> <br />
+            <button onClick={sendData}>Add Toddo</button>
         </div>
     )
 }
