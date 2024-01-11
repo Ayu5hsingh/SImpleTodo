c952ed93561509385dac1b59d3bc967aea4f32fe
Ayush Singh <heyayushsingh05@gmail.com>
2024-01-11 21:33:16 +0530

    Performance update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,14 @@
-import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Todos } from './components/Todos'
 import CreateTodo from './components/CreateTodo'
 import './components/components.css'
+import useTodo from './hooks/useTodo'
 
 
 function App() {
-  const [state, setTodo] = useState([])
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("http://localhost:3000/todos")
-        if (!response.ok) {
-          throw new Error("The server resfused the connection")
-        }
-        const json = await response.json()
-        setTodo(json)
-      } catch (e) {
-        console.log("Error : ", e)
-      }
-    }
-    fetchData()
-  },[state])
+  
+  const state = useTodo()
 
   return (
     <div className='page'>
